850eb3eaed4a942a7d2b44122175c6d9f56b0131
Victor Velásquez <vvelasquez@safefleet.net>
2020-09-26 17:59:14 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# product-availability-checker
+
+## Install  dependencies
+
+`pip3 install schedule requests pyyaml retry --user`
+
+## Configure environment variables
+
+In order to run the script you need to export the following environment variables:
+
+* `TELEGRAM_BOT_TOKEN`: Is the token of the bot you will use, please see section "Creating your bot Section"
+* `TELEGRAM_BOT_CHAT_ID`: The id of the chat where the bot will send messages, please see section "Creating your bot Section"
+
+## Creating your bot
+
+On Telegram, search @BotFather, send him a "/start" message
+Send another "/newbot" message, then follow the instructions to setup a name and a username
+Your bot is now ready, be sure to save a backup of your API token, and correct, this API token is your bot_token.
+Export it as your `TELEGRAM_BOT_TOKEN` environment variable
+
+## Configure bot
+
+On Telegram, search your bot (by the username you just created), press the “Start” button or send a “/start” message.
+
+Open a new tab with your browser, enter https://api.telegram.org/bot<yourtoken>/getUpdates , replace <yourtoken> with your API token, press enter and you should see something like this:
+{"ok":true,"result":[{"update_id":77xxxxxxx,
+"message":{"message_id":550,"from":{"id":34xxxxxxx,"is_bot":false,"first_name":"Man Hay","last_name":"Hong","username":"manhay212","language_code":"en-HK"}
+
+Look for “id”, for instance, 34xxxxxxx above is my chat id. Look for yours and export it as your `TELEGRAM_BOT_CHAT_ID` environment variable.
+
+## Configure Products
+
+Edit `products.yml` and add the products you want to check.
+
+## Run the tool
+
+`./product_checker.py`
---
+++ product_checker.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python3
+
+import sys
+from lxml import html
+import requests
+from time import sleep
+import time
+import schedule
+import logging
+import yaml
+import urllib.parse
+import random
+from retry import retry
+import os
+
+## Logging Config
+logging.basicConfig(format='%(asctime)s :: %(levelname)s :: %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+## Validations
+botToken = 'TELEGRAM_BOT_TOKEN'
+botChatId = 'TELEGRAM_BOT_CHAT_ID'
+if botToken not in os.environ or botChatId not in os.environ:
+  logger.error("Environment variables TELEGRAM_BOT_TOKEN and TELEGRAM_BOT_CHAT_ID are mandatory.")
+  exit(1)
+
+if len(sys.argv) > 1:
+  products_file = sys.argv[1]
+else:
+  products_file = "products.yml"
+
+if (not os.path.isfile(products_file)):
+  logger.error(f'Products database {products_file} does not exists.')
+  exit(1)
+
+logger.info("Using products.yml as products database.")
+
+def sendMessage(bot_message):
+  bot_message = bot_message.replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("`", "\\`")
+  bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
+  bot_chatID = os.environ.get('TELEGRAM_BOT_CHAT_ID')
+  send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + urllib.parse.quote(bot_message)
+
+  response = requests.get(send_text)
+  logger.debug("Telegram response: ")
+
+  json_response = response.json()
+  if json_response["ok"] == False:
+
+    logger.error(f'Error sending telegram message: {json_response}')
+
+  return json_response
+
+def sendAvailableProductMessage(product):
+  logger.info(f'Sending Available Message!!! >> {product["Url"]}')
+  return sendMessage(f'The product {product["Url"]} is available! HURRY UP!!')
+
+def check_availability(store, productUrl):
+  randomInt =random.randint(1, 100)
+  headers = ({'User-Agent':
+            f'Mozilla/5.0 (X11; Linux x86_64{randomInt}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
+
+  if store == "Amazon":
+    xpath_availability = '//div[@id ="availability"]//text()'
+  elif store == "NewEgg":
+    xpath_availability = '//div[@id ="availability"]//text()'
+    logger.error("Newegg is not supported yet...")
+    return False
+  else:
+    logger.info(f'Unknown store {store}.')
+    return False
+
+  # Checking availability
+  logger.debug(f'Loading {productUrl}')
+  page = requests.get(productUrl, headers = headers)
+
+  # parsing the html content
+  doc = html.fromstring(page.content)
+  logger.debug(f'response: {page.content}')
+
+  raw_availability = doc.xpath(xpath_availability)
+  availability = ''.join(raw_availability).strip().replace('\n', '') if raw_availability else None
+  logger.debug(f'Product Availability: {availability}')
+  if availability is not None:
+    return availability.lower().find("unavailable") == -1
+  else:
+    logger.error(f'We have a problem scrapping this url: {productUrl}, sending Message')
+    logger.error(f'{page.content}')
+    sendMessage(f'Error parsing {productUrl}, please check!')
+    raise Exception('Error Parsing', f'product {productUrl}')
+
+  return False
+
+# scheduling same code to run multiple
+# times after every 1 minute
+@retry(delay=10, backoff=2, logger=logger, max_delay=300)
+def job():
+  print("-------------------------------------------------------------------------------------------")
+  logger.info("Loading products from {products}".format(products=products_file))
+  with open(products_file) as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    products = yaml.load(file, Loader=yaml.FullLoader)
+
+  logger.info("Starting Tracking....")
+  for store in products:
+    i = 0
+    total = len(products[store])
+    logger.info(f'Working with {store}, tracking {total} products.')
+    for product in products[store]:
+      i = i+1
+      logger.info(f'Checking availability of product {product["Name"] if "Name" in product else product["Url"]} ({i} of {total}).')
+      result = check_availability(store, product["Url"])
+      if result:
+        logger.info("Product AVAILABLE HURRY UP!!!")
+        sendAvailableProductMessage(product)
+      else:
+        logger.info("Product unavailable :(...")
+
+      # because continuous checks in milliseconds or few seconds blocks your request
+      sleep(2)
+    #ReadAsin()
+
+# Run the job The first time, then schedule next executions.
+job();
+schedule.every(30).seconds.do(job)
+
+while True:
+
+    # running all pending tasks/jobs
+    schedule.run_pending()
+    time.sleep(1)
